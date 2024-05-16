486f5ada7515259c048e3e156dcff66b67a2fc5b
Durgadeviswaroopabikkina <durgadevibikkina123@gmail.com>
2024-05-16 13:03:19 +0530

    adding todolist

--- main.js
+++ main.js
@@ -1,67 +1,59 @@
 document.addEventListener("DOMContentLoaded",()=>{
-const storedToDo = JSON.parse(localStorage.getItem("todosList"));
-if(storedToDo){
-    storedToDo.forEach((todo) => {
-        addToDoList(todo);
-    });
-};
+    const storedToDo =JSON.parse(localStorage.getItem("todoslist"));
+    if(storedToDo){
+        storedToDo.forEach((todo)=>{
+            addToDoList(todo);
+        });
+    };
 });
-const input = document.querySelector(".input");
+ const text = document.querySelector(".text");
 const button = document.querySelector(".button");
-const lists = document.querySelector(".lists");
-
+const list = document.querySelector(".list");
 function addToDoList(todo){
     const div = document.createElement("div");
     const li = document.createElement("li");
+    
     div.classList.add("todo-div");
     li.classList.add("todo-li");
-    li.innerText = todo.text;
+    li.innerText = todo.text
     div.appendChild(li);
-    li.appendChild(div);
-
+    list.appendChild(div);
     const deleteButton = document.createElement("button");
     const editButton = document.createElement("button");
     editButton.innerHTML = "Edit";
     deleteButton.innerHTML = "Delete";
     div.appendChild(editButton);
     div.appendChild(deleteButton);
-
     deleteButton.addEventListener("click",()=>{
         div.parentNode.removeChild(div);
         updateToDoList();
     });
-
     editButton.addEventListener("click",()=>{
-        const editInput = document.createElement("input");
+        const editInput = document.createElement("Input");
         editInput.type = "text";
         editInput.value = li.innerText.trim();
-        li.innerHTML = "";
+        li.innerHTML = ""; //clear the list item
         li.appendChild(editInput);
         editInput.addEventListener("keypress",(e)=>{
-            if(e.key === "Enter"){
+            if(e.key=== "Enter"){
                 li.innerText = editInput.value;
                 updateToDoList();
             }
         });
     });
 };
- 
 function updateToDoList(){
-    const todos = Array.from(lists.querySelectorAll(".todo-li")).map((item)=>{
+    const todos = Array.from(list.querySelectorAll(".todo-li")).map((item)=>{
         return{text:item.innerText.trim()};
     });
-    localStorage.setItem("todosList",JSON.stringify(todos));
-}
-button.addEventListener("click",(e)=>{
-    e.preventDefault();
-    if(input.value.trim() !== ""){
-        const todo = {text:input.value.trim()};
+    localStorage.setItem("todoslist",JSON.stringify(todos));
+};
+ button.addEventListener("click", (e) => {
+     e.preventDefault();
+     if(text.value.trim()!==""){
+        const todo = {text: text.value.trim()};
         addToDoList(todo);
         updateToDoList();
-        input.value="";
+        text.value = "";
     };
 });
-
-       
-
-   
